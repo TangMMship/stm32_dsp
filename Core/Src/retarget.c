b601c7b096d9d43b7dbea9fb64d9e82b06641602
TangM <1830162050@qq.com>
2023-07-20 13:25:14 +0800

    dsp初始化

---
+++ Core/Src/retarget.c
@@ -0,0 +1,44 @@
+#include <_ansi.h>
+#include <_syslist.h>
+#include <errno.h>
+#include <sys/time.h>
+#include <sys/times.h>
+#include <retarget.h>
+#include <stdint.h>
+
+#if !defined(OS_USE_SEMIHOSTING)
+
+#define STDIN_FILENO  0
+#define STDOUT_FILENO 1
+#define STDERR_FILENO 2
+UART_HandleTypeDef *gHuart;
+
+void RetargetInit(UART_HandleTypeDef *huart)
+{
+    gHuart = huart;
+
+    /* Disable I/O buffering for STDOUT stream, so that
+     * chars are sent out as soon as they are printed. */
+    setvbuf(stdout, NULL, _IONBF, 0);
+}
+
+int _isatty(int fd)
+{
+    if (fd >= STDIN_FILENO && fd <= STDERR_FILENO)
+        return 1;
+
+    errno = EBADF;
+    return 0;
+}
+
+int _close(int fd)
+{
+    if (fd >= STDIN_FILENO && fd <= STDERR_FILENO)
+        return 0;
+
+    errno = EBADF;
+    return -1;
+}
+
+
+#endif //#if !defined(OS_USE_SEMIHOSTING)
